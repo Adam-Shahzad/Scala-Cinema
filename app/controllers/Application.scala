18b58a2af3779fe21519479468ee87f980acc2b4
fahofah <fahriulucaycy@gmail.com>
2017-11-14 09:08:26 +0000

    Day2 Start

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,8 @@
 package controllers
 
 import javax.inject.Inject
-import models.Payment
+
+import models.{Payment, SeatSelection}
 import play.api._
 import play.api.mvc._
 import reactivemongo.bson.BSONDocument
@@ -12,7 +13,10 @@ import scala.concurrent.Future
 
 class Application  @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-  var seatList = ArrayBuffer[String]
+  var seatList = scala.collection.mutable.Map[Int, Char]()
+  val seatLetters = ('A' to 'F').toList
+  val rowNumbers = (1 to 10).toList
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
@@ -76,9 +80,21 @@ class Application  @Inject() (val messagesApi: MessagesApi) extends Controller w
   }
 
   def seatSelection = Action {
-    val seatLetters = ('A' to 'F').toList
-    val rowNumbers = (1 to 10).toList
-    Ok(views.html.seatSelection(seatLetters, rowNumbers, seatList))
+    val initialise = SeatSelection(List(true,false))
+    SeatSelection.seatList.append(initialise)
+    Ok(views.html.seatSelection(SeatSelection.createForm, SeatSelection.seatList, "start"))
+  }
+
+  def seatSelectionForm = Action{ implicit request =>
+    val retriveSeatsForm = SeatSelection.createForm.bindFromRequest()
+   retriveSeatsForm.fold({formWithErrors =>
+      BadRequest(views.html.seatSelection(SeatSelection.createForm, SeatSelection.seatList,"bad"))
+    },{seats =>
+     val toAdd = SeatSelection(seats.seat)
+      SeatSelection.seatList.append(toAdd)
+      Ok(views.html.seatSelection(SeatSelection.createForm, SeatSelection.seatList, "good"))
+    })
+
   }
 
 }
