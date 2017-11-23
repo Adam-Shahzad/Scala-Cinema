881c85696a8a0436d71a5c4baafaf60402b3248f
fahofah <fahriulucaycy@gmail.com>
2017-11-23 12:57:25 +0000

    Merge branch 'Developer' of https://github.com/Adam-Shahzad/Scala-Cinema into actuallyBookMovie
    # Conflicts:
    #	app/controllers/LoginController.scala
    #	app/controllers/TicketBookingController.scala
    #	app/views/individualMovie.scala.html
    #	app/views/listingsGallery.scala.html
    #	app/views/logIn.scala.html
    #	app/views/ticketSelection.scala.html
    #	conf/routes

--- app/models/TicketBooking.scala
+++ app/models/TicketBooking.scala
@@ -2,7 +2,7 @@ package models
 import play.api.data._
 import play.api.data.Forms._
 
-case class TicketBooking(bookerName: String, bookerEmail: String, movieTime: Option[String] = None, adultTicket: Option[Int] = None, childTicket: Option[Int] = None, studentTicket: Option[Int]= None, concessionTicket: Option[Int]= None) {
+case class TicketBooking(bookerName: String, bookerEmail: String, movieTime: Option[String] = None, adultTicket: Option[Int] = None, childTicket: Option[Int] = None, concessionTicket: Option[Int]= None) {
 
 }
 
@@ -14,7 +14,6 @@ object  TicketBooking {
       "movieTime" -> optional(text),
       "adultTicket" ->  optional(default(number, 0)),
       "childTicket" -> optional(default(number, 0)),
-      "studentTicket" -> optional(default(number, 0)),
       "concessionTicket" -> optional(default(number, 0))
 
     )(TicketBooking.apply)(TicketBooking.unapply)
