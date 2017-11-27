9e2bd9114dadb034e1117eb0f154f1c2f2eb408c
Damien <damienbutterworth@hotmail.co.uk>
2017-11-27 10:40:36 +0000

    Started Testing

--- app/models/Discussion.scala
+++ app/models/Discussion.scala
@@ -3,14 +3,33 @@ package models
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.format.Formats._
+import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
 
 case class Discussion (name: String, email:String, desc:String, filmName:String, rating:Double)
 
 object Discussion{
 
+  val allNumber = """\d*""".r
+  val allLetters = """[A-Za-z]*""".r
+
+  val checkContstraint: Constraint[String] = Constraint("constraints.passwordcheck")({
+    plainText =>
+      val errors = plainText match {
+        case allNumber() => Seq(ValidationError("Password is all numbers"))
+        case allLetters() => Seq(ValidationError("Password is all characters"))
+        case _ => Nil
+      }
+      if (errors.isEmpty)
+        Valid
+      else
+          Invalid(errors)
+  })
+
+
+
   val createForm = Form(
     mapping (
-      "name" -> nonEmptyText,
+      "name" -> nonEmptyText.verifying(checkContstraint),
       "email" -> email,
       "desc" -> nonEmptyText(minLength = 50, maxLength = 500),
       "filmName" -> nonEmptyText,
