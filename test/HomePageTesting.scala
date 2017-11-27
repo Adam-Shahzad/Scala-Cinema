1b65a7fb5683dbc72369b64fa1b49a1ef44657d0
mariannepearson0 <mariannepearson@gmail.com>
2017-11-27 10:46:04 +0000

    init

--- test/HomePageTesting.scala
+++ test/HomePageTesting.scala
@@ -3,7 +3,7 @@ import org.scalatestplus.play._
 
 class HomePageTesting extends PlaySpec {
 
-  "clicking the listings gallery" should "navigate the user to the listings gallery page" in {
+  "clicking the listings gallery" should "navigae the user to the listings gallery page" in {
 
 
 
