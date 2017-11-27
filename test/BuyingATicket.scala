36397a53cc1cc959edf655f514e66b4a29788103
mariannepearson0 <mariannepearson@gmail.com>
2017-11-27 16:40:59 +0000

    pushing after testing

---
+++ test/BuyingATicket.scala
@@ -0,0 +1,33 @@
+import org.scalatest._
+import selenium._
+
+class BuyingATicket extends FlatSpec with Matchers with HtmlUnit {
+
+  val host = "http://localhost:9000/"
+
+  "Clicking on the listings Gallery button" should "navigate user to the listings gallery" in {
+    go to (host)
+    click on id("listingsGallery")
+    pageTitle should be("Listing Gallery")
+  }
+  it should "then go to ticket selection page after clicking book movie button" in {
+    click on id("BookMovie0")
+    pageTitle should be("Ticket Selection")
+  }
+  it should "after fields are filled in and select button pressed take you to the payment page" in {
+    textField("bookerName").value = "Marianne"
+    textField("bookerEmail").value = "mariannepearson0@gmail.com"
+    textField("adultTicket").value = "2"
+    click on id("selectButton")
+    pageTitle should be ("Payment")
+  }
+  it should "after filling in payment details and pressing make payment, go to successful payment page" in {
+    textField("name").value = "Marianne"
+    textField("number").value = "1234567891012345"
+    textField("expiry").value = "08/21"
+    textField("csv").value = "123"
+    click on id("paymentButton")
+    pageTitle should be ("Payment Confirmation")
+    id("payConfMessage").webElement.getText should include ("mariannepearson0@gmail.com")
+  }
+}
--- test/HomePageTesting.scala
+++ test/HomePageTesting.scala
@@ -1,8 +1,7 @@
-
 import org.scalatest._
 import selenium._
 
-class BlogSpec2 extends FlatSpec with Matchers with HtmlUnit {
+class HomePageTesting extends FlatSpec with Matchers with HtmlUnit {
 
   val host = "http://localhost:9000/"
 
@@ -16,6 +15,27 @@ class BlogSpec2 extends FlatSpec with Matchers with HtmlUnit {
     click on id("listingsGallery")
     pageTitle should be ("Listing Gallery")
   }
+
+  "Clicking on the New Releases button" should "navigate user to the new releases gallery" in {
+    go to (host)
+    click on id("newReleases")
+    pageTitle should be ("New Releases")
+  }
+
+  "Clicking on the My Ticket Bookings button" should "navigate user to the ticket booking page" in {
+    go to (host)
+    click on id("ticketBookings")
+    pageTitle should be ("Log In")
+  }
+
+  "Clicking on a poster of a movie" should "take you to the individual movie page for it" in {
+    go to (host)
+    click on id("posterClick")
+    pageTitle should be ("Individual Movie")
+  }
+
+  "The bottom of the home page" should "have a list of offers at the cinema" in {
+    go to (host)
+    id("greatOffers").webElement.getText should include ("Great Offers")
+  }
 }
-
-
